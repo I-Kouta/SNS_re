edfd63d562cf0ee2901e834f0502ba47dea0e534
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-26 16:57:21 +0900

    アドレス以外のバリデーション記述

--- app/Http/Requests/ProfileUpdateFormRequest.php
+++ app/Http/Requests/ProfileUpdateFormRequest.php
@@ -26,10 +26,32 @@ class ProfileUpdateFormRequest extends FormRequest
         return [
             'username' => 'required|string|min:2|max:12',
             // 自分のアドレスはバリデーション対象から除外する必要がある
-            'mail' => 'required|string|email|min:5|max:40|unique:users,mail,'.$request->id.',id',
+            'mail' => 'required|string|email|min:5|max:40|unique:users,mail,',
             'password' => 'required|string|min:8|max:20|confirmed',
             'bio' => 'max:150',
             'images' => 'mimes:jpg, png, bmp, gif, svg'
+        ];
+    }
+
+    public function messages(){
+        return [
+            "username.required" => "名前の入力は必須です",
+            "username.string" => "名前の入力された内容の形式が異なります",
+            "username.min" => "名前は2文字以上での入力が必須です",
+            "username.max" => "名前は12文字以内での入力が必須です",
+            "mail.required" => "アドレスの入力は必須です",
+            "mail.string" => "入力されたアドレスの内容の形式が異なります",
+            "mail.email" => "アドレスの形式で入力されていません",
+            "mail.min" => "アドレスは5文字以上での入力が必須です",
+            "mail.max" => "アドレスは40文字以内での入力が必須です",
+            "mail.unique" => "すでに使用されているアドレスです",
+            "password.required" => "パスワードの入力は必須です",
+            "password.string" => "パスワードの入力された内容の形式が異なります",
+            "password.min" => "パスワードは8文字以上での入力が必須です",
+            "password.max" => "パスワードは20文字以内での入力が必須です",
+            "password.confirmed" => "パスワードが一致していません",
+            "bio.max" => "紹介文は150字以内で入力してください"
+            "images.mines" => "この形式の画像は保存できません"
         ];
     }
 }
